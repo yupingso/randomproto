60a8096b5e614b94bf93db5ce8c98dfdf5195b17
yupingso <ypwu@bridgewell.com>
2019-01-16 16:12:06 +0800

    [config] Add long description and keywords

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setuptools.setup(
     name='randomproto',
-    version='0.0.1rc1',
+    version='0.0.1rc2',
     py_modules=('randomproto',),
     install_requires=[
         'protobuf>=3.6.0',
@@ -11,6 +11,9 @@ setuptools.setup(
     author='Yu-Ping Wu',
     author_email='yupingso@gmail.com',
     description='Random protobuf object generator',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    keywords='protobuf proto message random generate generator',
     url='https://github.com/yupingso/randomproto',
     classifiers=[
         'Programming Language :: Python :: 2.7',
